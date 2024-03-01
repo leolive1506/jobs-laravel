387ad2a2cfd88219b5d3e0cab006aca4ce04edba
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-01 14:15:43 -0300

    create command and notification

---
+++ app/Console/Commands/SendNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\SantamJobTop;
+use Illuminate\Console\Command;
+
+class SendNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send-notification';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // User::all()->each(fn (User $user) => $user->notify());
+        $this->withProgressBar(
+            User::all(),
+            fn (User $user) => $user->notify(new SantamJobTop())
+        );
+    }
+}
